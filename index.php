6ac9cd0b17e8a2788ffa636f13e5f19b51d17ca0
Alina <stereoalina@gmail.,com>
2019-06-29 15:38:46 +0300

    подключение ReadBeanPHP

--- index.php
+++ index.php
@@ -11,8 +11,19 @@ session_start();
 // 2. Подключение файлов системы
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/components/Autoload.php');
+require_once(ROOT.'/libs/rb.php');
 
-// 3. Вызов Router
+// 3. Подключение к базе данных
+// установить соединение с базой данных
+R::setup('mysql:host=127.0.0.1;dbname=comp', 'root', '');
+// логика авто-создания полей и таблиц
+R::freeze(true);
+// проверка подключения
+if (!R::testConnection()) {
+	exit('Нет подключения к БД');
+}
+
+// 4. Вызов Router
 $router = new Router();
 $router->run();
 

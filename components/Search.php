18523aadd65db3546c07e3b5bd8a635d6a2dd9b4
Alina <stereoalina@gmail.,com>
2019-06-30 16:20:12 +0300

    Изменен поиск

--- components/Search.php
+++ components/Search.php
@@ -12,52 +12,35 @@ class Search
 	}
 
 	public static function getTotalProductsSearch($keyword) {
-		
-		$db = Db::getConnection();
 
-    	$result = $db->query("SELECT count(id) AS count FROM product "
-    		. "WHERE status='1' AND name LIKE '%" . $keyword . "%' "
-	        . "OR code LIKE '%" . $keyword . "%' "
-	        . "OR brand LIKE '%" . $keyword . "%' "
-	        . "OR description LIKE '%" . $keyword . "%'");
-    	$result->setFetchMode(PDO::FETCH_ASSOC);
-    	$row = $result->fetch();
-
-    	return $row['count'];
+    	return R::count( 'product', ' status = 1 AND (name LIKE ?'
+    		. ' OR code LIKE ?'
+    		. ' OR brand LIKE ?'
+    		. ' OR description LIKE ?)', 
+    	array ( '%'.$keyword.'%', 
+    			'%'.$keyword.'%', 
+    			'%'.$keyword.'%', 
+    			'%'.$keyword.'%') );
 	}
 	
     public static function searchProducts($keyword, $page = 1) {
 
-    	if ($keyword) {
-    		$page = intval($page);
-	    	$offset = ($page - 1) * self::SHOW_BY_DEFAULT;
+    	$page = intval($page);
+    	$offset = ($page - 1) * self::SHOW_BY_DEFAULT;
 
-	        $db = Db::getConnection();
-
-	        $products = array();
-
-	        $result = $db->query("SELECT * FROM product "
-		        . "WHERE status = '1' "
-		        . "AND name LIKE '%" . $keyword . "%' "
-		        . "OR code LIKE '%" . $keyword . "%' "
-		        . "OR brand LIKE '%" . $keyword . "%' "
-		        . "OR description LIKE '%" . $keyword . "%' "
-		        . "ORDER BY id DESC "
-		        . "LIMIT " . self::SHOW_BY_DEFAULT
-				. " OFFSET " . $offset);
-
-	    	$i = 0;
-	    	while ($row = $result->fetch()) {
-	    		$products[$i]['id'] = $row['id'];
-	    		$products[$i]['name'] = $row['name'];
-	    		$products[$i]['code'] = $row['code'];
-	    		$products[$i]['brand'] = $row['brand'];
-	    		$products[$i]['price'] = $row['price'];
-	    		$products[$i]['description'] = $row['description'];
-	    		$i++;
-	    	}
-	    	return $products;
-    	}
+    	return R::findAll( 'product', ' `status` = 1 AND (`name` LIKE ?'
+    		. ' OR `code` LIKE ?'
+    		. ' OR `brand` LIKE ?'
+    		. ' OR `description` LIKE ?)'
+    		. ' ORDER BY `id` DESC'
+    		. ' LIMIT ?'
+    		. ' OFFSET ?', 
+    	array ( '%'.$keyword.'%', 
+    			'%'.$keyword.'%', 
+    			'%'.$keyword.'%', 
+    			'%'.$keyword.'%',
+    			self::SHOW_BY_DEFAULT,
+    			$offset) );
     }
 }
 
